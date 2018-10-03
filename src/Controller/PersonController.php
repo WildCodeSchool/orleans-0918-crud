f1bc2ef2305883ef2edaf65047be097542c83e27
Sylvain Blondeau <sylvain@wildcodeschool.fr>
2018-10-03 10:04:48 +0200

    mise en place de twig

--- src/Controller/PersonController.php
+++ src/Controller/PersonController.php
@@ -3,15 +3,31 @@
 namespace App\Controller;
 
 use App\Model\PersonManager;
+use Twig_Environment;
+use Twig_Loader_Filesystem;
 
 class PersonController
 {
+    private $twig;
+
+    public function __construct()
+    {
+        $loader = new Twig_Loader_Filesystem(__DIR__ . '/../View');
+        $this->twig = new Twig_Environment($loader, [
+                'cache' => false,
+            ]
+        );
+    }
+
     public function list()
     {
         $personManager = new PersonManager();
         $persons = $personManager->selectPersons();
 
-        require __DIR__ . '/../View/list.php';
+        echo $this->twig->render('list.html.twig', [
+                'persons' => $persons,
+            ]
+        );
     }
 
     public function show(int $id)
